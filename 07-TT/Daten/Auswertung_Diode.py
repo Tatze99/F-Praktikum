b6e4223cde5e539b83636fbed174acaf02b2d0f0
Martin Beyer <martinbeyer99@googlemail.com>
2020-08-26 17:51:59 +0200

    TT - Aufgabe 2 fertig

---
+++ 07-TT/Daten/Auswertung_Diode.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+
+Spektrumfile = input("Enter file name: ")
+
+Kalibrierung = np.loadtxt("Kalibrierung_Diode.csv", delimiter=';')
+Spektrum = np.loadtxt(Spektrumfile)
+S = interp1d(Kalibrierung[:,0], Kalibrierung[:,1], kind='cubic')
+Spektrum_new = S(Spektrum[:,1])
+
+np.savetxt("Kalibrierung_"+Spektrumfile, np.c_[Spektrum[:,0], Spektrum_new], "%.9f")
